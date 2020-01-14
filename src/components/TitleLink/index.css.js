d94070c67ea6c84aa14ebf352d0fc01afedbe602
Illia Tsalko <illia.tsalko@gmail.com>
2020-01-14 04:51:51 +0200

    fixed title link

--- src/components/TitleLink/index.css.js
+++ src/components/TitleLink/index.css.js
@@ -1,16 +1,52 @@
 import styled, { createGlobalStyle } from 'styled-components';
 import { screens, fonts, colors } from '../../constants/theme';
 
-export const TitleLinkCss = styled.div`
+export const OverlayCss = styled.div`
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	position: fixed;
+	z-index: 999;
+	background: ${colors.black};
+	opacity: 0.8;
+	display: ${(props) => (props.show ? 'block' : 'none')};
+	@media (min-width: ${screens.lg}) {
+		display: none;
+	}
+`;
 
-h3{
-	border-bottom:2px solid ${colors.white};
-	padding-bottom:5px;
-	display:inline-block;
-	width:auto;
-	cursor:pointer;
-}	
-	text-decoration:none;
-	
+export const TitleLinkCss = styled.div`
+	h3 {
+		border-bottom: 2px solid ${colors.grey};
+		padding-bottom: 5px;
+		display: inline-block;
+		width: fit-content;
+		cursor: pointer;
+	}
+	text-decoration: none;
+
 	
 `;
+
+export const ImgCss = styled.div`
+		overflow:hidden;
+		display: ${(props) => (props.show ? 'block' : 'none')};
+		position: absolute;
+		z-index: 9999;
+		
+		transform: translate(-102%, -30px);
+		@media (max-width: ${screens.lg}) {
+			width: 100%;
+			position: fixed;
+			left: 0;
+			top: 50%;
+			transform: translate(0, -50%);
+		}
+
+		img{
+			width:100%;
+		}
+
+
+`
--- src/components/TitleLink/index.js
+++ src/components/TitleLink/index.js
@@ -1,12 +1,22 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import {TitleLinkCss} from "./index.css"
+import React,{useState} from "react";
+import {TitleLinkCss,OverlayCss,ImgCss} from "./index.css"
 
 
-const TitleLink = ({children}) => {
+const TitleLink = (props) => {
+
+  const [show,setShow] = useState(false);
+
   return (
-   <TitleLinkCss>
-    <h3>{children}</h3>
+   <TitleLinkCss >
+    <h3 onMouseOver={()=>setShow(true)} onMouseOut={()=>setShow(false)} onTouchEnd={()=>setShow(true)}>{props.children}</h3>
+    {props.img&&(
+      <>
+        <ImgCss show={show}><img src={props.img} alt={props.children}/> </ImgCss>
+      <OverlayCss show={show} onTouchEnd={()=>setShow(false)}>&nbsp;</OverlayCss>
+      </>
+    )            
+    }
+
    </TitleLinkCss>
   );
 };
