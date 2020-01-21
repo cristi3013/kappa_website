3571dbdbaf60dfcf1ba854c43b087ada4d80ac51
Illia Tsalko <illia.tsalko@gmail.com>
2020-01-21 15:06:00 +0200

    replaced videos with gifs

--- src/components/Bryant/section4/index.js
+++ src/components/Bryant/section4/index.js
@@ -1,15 +1,19 @@
 import React, { useRef, useEffect } from 'react';
-import { BryantSectionWrapperCss, InfoBlockCss, InfoBlockItemCss, ItemsCss, ImageCss,VideoCss } from './index.css';
+import { BryantSectionWrapperCss, InfoBlockCss, InfoBlockItemCss, ItemsCss, ImageCss, VideoCss } from './index.css';
 import { SecondaryLinkCss } from '../../../layout/index.css';
 
 import { useInView } from 'react-intersection-observer';
 
-import video1 from '../videos/2.mp4';
-import video2 from '../videos/3.mp4';
-import video3 from '../videos/4.mp4';
+import video1 from '../videos/2.gif';
+import picture1 from '../videos/2.png';
+import video2 from '../videos/3.gif';
+import picture2 from '../videos/3.png';
+import video3 from '../videos/4.gif';
+import picture3 from '../videos/4.png';
+
+
 
 const BryantSection2 = () => {
-
 	const video1_ref = useRef(null);
 	const video2_ref = useRef(null);
 	const video3_ref = useRef(null);
@@ -28,15 +32,30 @@ const BryantSection2 = () => {
 
 	useEffect(
 		() => {
-			if (video1_view) {video1_ref.current.play();} else {video1_ref.current.pause(); video1_ref.current.currentTime=0;}
-			if (video2_view) {video2_ref.current.play();} else {video2_ref.current.pause(); video2_ref.current.currentTime=0;}
-			if (video3_view) {video3_ref.current.play();} else {video3_ref.current.pause(); video3_ref.current.currentTime=0;}
+			if (video1_view) {
+				video1_ref.current.src=video1;
+			} else {
+				video1_ref.current.src=picture1;
+				
+			}
 
+			if (video2_view) {
+				video2_ref.current.src=video2;
+			} else {
+				video2_ref.current.src=picture2;
+				
+			}
+			if (video3_view) {
+				video3_ref.current.src=video3;
+			} else {
+				video3_ref.current.src=picture3;
+				
+			}
 
+		
 		},
-		[ video1_view,video2_view,video3_view ]
+		[ video1_view, video2_view, video3_view ]
 	);
-
 
 	return (
 		<BryantSectionWrapperCss>
@@ -46,15 +65,16 @@ const BryantSection2 = () => {
 					experience.
 				</p>
 			</InfoBlockCss>
-			<VideoCss ref={video1_wrapper}>
-				<video src={video1} crossOrigin="true" loop ref={video1_ref} />
-			</VideoCss>
-			<VideoCss  ref={video2_wrapper}>
-				<video src={video2} crossOrigin="true" loop ref={video2_ref}/>
-			</VideoCss>
-			<VideoCss  ref={video3_wrapper}>
-				<video src={video3} crossOrigin="true" loop ref={video3_ref}/>
-			</VideoCss>
+			<ImageCss ref={video1_wrapper}>
+				<img src={picture1} ref={video1_ref} />
+			</ImageCss>
+			<ImageCss ref={video2_wrapper}>
+				<img src={picture2} ref={video2_ref} />
+			</ImageCss>
+			<ImageCss ref={video3_wrapper}>
+				<img src={picture3} ref={video3_ref} />
+			</ImageCss>
+			
 		</BryantSectionWrapperCss>
 	);
 };
