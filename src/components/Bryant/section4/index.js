d2083d080e85dc69fe494b5e3d3f82ac18fa1eee
Illia Tsalko <illia.tsalko@gmail.com>
2020-01-23 11:42:24 +0200

    ios video fix

--- src/components/Bryant/section4/index.js
+++ src/components/Bryant/section4/index.js
@@ -4,10 +4,28 @@ import { SecondaryLinkCss } from '../../../layout/index.css';
 
 import { useInView } from 'react-intersection-observer';
 import video1 from '../videos/2.mp4';
+import gif1 from '../videos/2.gif';
 import video2 from '../videos/3.mp4';
 import video3 from '../videos/4.mp4';
 
 
+
+const playVideo = (video) =>{
+
+	var promise = video.play();
+
+		if (promise !== undefined) {
+			promise.catch(error => {
+
+				video.controls = true;
+				
+			}).then(() => {
+				
+				//promise.play()
+			});
+		}
+
+}
 
 const BryantSection2 = () => {
 	const video1_ref = useRef(null);
@@ -29,25 +47,10 @@ const BryantSection2 = () => {
 	useEffect(
 		() => {
 
-			var promise;
-			if (video1_view) {promise=video1_ref.current;}else{video1_ref.current.pause(); video1_ref.current.currentTime=0;}
-			if (video2_view) {promise=video2_ref.current;}else{video2_ref.current.pause(); video2_ref.current.currentTime=0;}
-			if (video3_view) {promise=video3_ref.current;}else{video3_ref.current.pause(); video3_ref.current.currentTime=0;}
-
-			if (promise&&promise.play() !== undefined) {
-				promise.play().catch(error => {
-					promise.controls="true";
-					promise.muted = "true";
-					promise.playsinline = "true";
-					promise.autoplay = "true";
-				}).then(() => {
-					
-					//promise.play()
-				});
-			}
-
-
-	
+		
+			if (video1_view) {playVideo(video1_ref.current);}else{video1_ref.current.pause(); video1_ref.current.currentTime=0;}
+			if (video2_view) {playVideo(video2_ref.current);}else{video2_ref.current.pause(); video2_ref.current.currentTime=0;}
+			if (video3_view) {playVideo(video3_ref.current);}else{video3_ref.current.pause(); video3_ref.current.currentTime=0;}
 
 		
 		},
@@ -63,13 +66,14 @@ const BryantSection2 = () => {
 				</p>
 			</InfoBlockCss>
 			<VideoCss ref={video1_wrapper}>
-				<video src={video1} crossOrigin="true" loop ref={video1_ref} />
+				<video src={video1} crossOrigin="true" autoplay loop muted playsinline ref={video1_ref} />
+
 			</VideoCss>
 			<VideoCss  ref={video2_wrapper}>
-				<video src={video2} crossOrigin="true" loop ref={video2_ref}/>
+				<video src={video2} crossOrigin="true" loop playsinline muted ref={video2_ref}/>
 			</VideoCss>
 			<VideoCss  ref={video3_wrapper}>
-				<video src={video3} crossOrigin="true" loop ref={video3_ref}/>
+				<video src={video3} crossOrigin="true" loop playsinline muted ref={video3_ref}/>
 			</VideoCss>
 			
 		</BryantSectionWrapperCss>
