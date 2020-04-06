6ceb45c0c81338c464a55ea35bcb04311ed8000b
razvan-soare <soare.razvan94@gmail.com>
2020-04-06 17:07:57 +0100

    Scroll top on route change

--- src/router.js
+++ src/router.js
@@ -1,31 +1,54 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, useLocation } from "react-router-dom";
 
-import Homepage from './pages/homepage';
-import WhoWeAre from './pages/whoweare';
-import JoinUs from './pages/joinus';
-import Internship from './pages/internship';
-import Nova from './pages/nova';
-import Bryant from './pages/bryant';
-import LayoutPath from './layout/index';
+import Homepage from "./pages/homepage";
+import WhoWeAre from "./pages/whoweare";
+import JoinUs from "./pages/joinus";
+import Internship from "./pages/internship";
+import Nova from "./pages/nova";
+import Bryant from "./pages/bryant";
+import LayoutPath from "./layout/index";
 
-import { LayoutsWrapperCss, GlobalStyle } from './layout/index.css';
+import { LayoutsWrapperCss, GlobalStyle } from "./layout/index.css";
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 export default function App() {
-	return (
-		<Router>
-			<LayoutsWrapperCss>
-				<GlobalStyle />
+  return (
+    <Router>
+      <ScrollToTop />
+      <LayoutsWrapperCss>
+        <GlobalStyle />
 
-				<Switch>
-					<LayoutPath exact path="/" component={Homepage} blocks={[ 1, 2, 3 ]} />
-					<LayoutPath path="/bryant" component={Bryant} project={true} blocks={[ 1, 2, 3 ]} />
-					<LayoutPath path="/nova" component={Nova} project={true} blocks={[ 1, 2, 3 ]} />
-					<LayoutPath path="/whoweare" component={WhoWeAre} blocks={[ 2, 3 ]} />
-					<LayoutPath path="/joinus" component={JoinUs} blocks={[ 2,1,3 ]} />
-					<LayoutPath path="/internship" component={Internship} blocks={[ 1, 3 ]} />
-				</Switch>
-			</LayoutsWrapperCss>
-		</Router>
-	);
+        <Switch>
+          <LayoutPath exact path="/" component={Homepage} blocks={[1, 2, 3]} />
+          <LayoutPath
+            path="/bryant"
+            component={Bryant}
+            project={true}
+            blocks={[1, 2, 3]}
+          />
+          <LayoutPath
+            path="/nova"
+            component={Nova}
+            project={true}
+            blocks={[1, 2, 3]}
+          />
+          <LayoutPath path="/whoweare" component={WhoWeAre} blocks={[2, 3]} />
+          <LayoutPath path="/joinus" component={JoinUs} blocks={[2, 1, 3]} />
+          <LayoutPath
+            path="/internship"
+            component={Internship}
+            blocks={[1, 3]}
+          />
+        </Switch>
+      </LayoutsWrapperCss>
+    </Router>
+  );
 }
