228e4d5111d300cba24a009f1090901a28657899
Cristian Vasile <vslcristian3013@gmail.com>
2020-10-29 00:09:32 +0200

    first section + folder structure

--- src/components/Internship/section1/index.css.js
+++ src/components/Internship/section1/index.css.js
@@ -1,63 +1,118 @@
 import styled from 'styled-components';
 import { screens, fonts, colors } from '../../../constants/theme';
 
-
-
 export const InternshipFirstSectionWrapperCss = styled.div`
+  display: flex;
+  padding-left: 8.33vw;
+  padding-top: 200px;
+  padding-bottom: 150px;
 
-    display:flex;
-    padding-left:8.33vw;
-    padding-top:200px;
-    padding-bottom:150px;
-    
-    align-items:flex-start;
+  align-items: flex-start;
 
-    h4{
-        width:16.66vw;
-        margin-right:8.33vw;
-        @media (max-width:${screens.lg}) {
-       width:100%;
-       margin-right:0px;
+  h4 {
+    width: 16.66vw;
+    margin-right: 8.33vw;
+    @media (max-width: ${screens.lg}) {
+      width: 100%;
+      margin-right: 0px;
     }
-    }
+  }
 
-    @media (max-width:${screens.lg}) {
-        flex-direction:column;
-        padding:50px 25px 25px;
-    }
+  @media (max-width: ${screens.lg}) {
+    flex-direction: column;
+    padding: 50px 25px 25px;
+  }
 `;
 
 export const InfoBlockCss = styled.div`
-    width:50vw;
-    @media (max-width:${screens.lg}) {
-       width:100%;
-       margin-bottom:100px;
-    }
+  width: 50vw;
+  @media (max-width: ${screens.lg}) {
+    width: 100%;
+    margin-bottom: 100px;
+  }
 
-    p{
-        span, a{
-            border-bottom:2px solid ${colors.blue};
-            text-decoration:none;
-        }
+  p {
+    span,
+    a {
+      border-bottom: 2px solid ${colors.blue};
+      text-decoration: none;
     }
+  }
 `;
 
 export const ItemsCss = styled.div`
-margin:140px 0px;
+  margin: 140px 0px;
 `;
 
-
 export const InfoBlockItemCss = styled.div`
-display:flex;
-flex-direction:column;
-margin-bottom:100px;
-img{
-    width:100%;
-    max-width:500px;
-    margin-bottom:50px;
-}
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 100px;
+  img {
+    width: 100%;
+    max-width: 500px;
+    margin-bottom: 50px;
+  }
 
-h1{
-    margin:0px 0px 5px;
-}
+  h1 {
+    margin: 0px 0px 5px;
+  }
+  .container {
+    position: relative;
+  }
+  .mask {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    top: 0;
+    left: 0;
+    height: 100%;
+    maxwidth: 100%;
+    height: 84%;
+  }
+`;
+export const PrimaryLinkCss = styled.div`
+  font-style: italic;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 23px;
+  letter-spacing: 0.03em;
+  text-decoration: none;
+  width: fit-content;
+  color: ${colors.white};
+  display: inline-block;
+
+  padding: 7px 7px 8px;
+  margin: -7px 25px 20px 0px;
+
+  text-align: center;
+
+  position: relative;
+  overflow: hidden;
+  transition: all 0.8s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+
+  &:after {
+    position: absolute;
+    transition: all 0.8s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+    content: '';
+    width: 100%;
+    right: auto;
+    left: 0;
+    bottom: 0;
+    height: 2px;
+    background: ${colors.blue};
+  }
+
+  &:hover {
+    cursor: pointer;
+    &:after {
+      width: 0%;
+      right: 0;
+      left: auto;
+    }
+  }
+
+  a {
+    text-decoration: none;
+  }
 `;
--- src/components/Internship/section1/index.js
+++ src/components/Internship/section1/index.js
@@ -1,90 +1,94 @@
 import React from 'react';
 import {
-	InternshipFirstSectionWrapperCss,
-	InfoBlockCss,
-	InfoBlockItemCss,
-	ItemsCss,
+  InternshipFirstSectionWrapperCss,
+  InfoBlockCss,
+  InfoBlockItemCss,
+  ItemsCss,
+  PrimaryLinkCss,
+  SvgMaskCss,
 } from './index.css';
 
 import gif1 from './images/1.gif';
 import gif2 from './images/2.gif';
 import gif3 from './images/3.gif';
 import gif4 from './images/4.gif';
+import mask from './images/mask.png';
 
 const InternshipFirstSection = () => {
-	return (
-		<InternshipFirstSectionWrapperCss>
-			<h4>
-				<span className="symbol">|</span> Intern with us{' '}
-				<span className="symbol">&lt;</span>
-			</h4>
-			<InfoBlockCss>
-				<h2>Interested in learning to code?</h2>
+  return (
+    <InternshipFirstSectionWrapperCss>
+      <h4>
+        <span className='symbol'>|</span> Intern with us{' '}
+        <span className='symbol'>&lt;</span>
+      </h4>
+      <InfoBlockCss>
+        <h1>You are the CSS to our HTML</h1>
+        <br></br>
+        <br></br>
+        <PrimaryLinkCss>
+          <a href='mailto:captain@kappa.london?subject=Application for Front-End Internship at Kappa London'>
+            Let's markup together
+          </a>
+        </PrimaryLinkCss>
+        <ItemsCss>
+          <InfoBlockItemCss>
+            <div className='container'>
+              <img src={gif1} />
+              <img src={mask} className='mask' />
+            </div>
 
-				<h2>
-					Join a team with the aim to wow the market by building dynamic and visually
-					appealing websites.
-				</h2>
+            <h1>Intense training</h1>
+            <p>
+              Our programme is developed around intense front-end training
+              techniques. You'll start off with exercises and tutorials that
+              cover in-depth HTML and CSS guidance.
+            </p>
+          </InfoBlockItemCss>
 
-				<h2>
-					We are always on the lookout for motivated students or recent graduates that
-					wish to take their skills to the next level. This is not a regular internship
-					but a bootcamp for the most determined individuals.
-				</h2>
-				<ItemsCss>
-					<InfoBlockItemCss>
-						<img src={gif1} />
-						<h1>Intense training</h1>
-						<p>
-							Our programme is developed around intense front-end training techniques.
-							You'll start off with exercises and tutorials that cover in-depth HTML
-							and CSS guidance.
-						</p>
-					</InfoBlockItemCss>
+          <InfoBlockItemCss>
+            <img src={gif2} />
+            <h1>Guaranteed growth</h1>
+            <p>
+              Then, you will be moving on to learning JavaScript by working
+              closely with very experienced engineers. A senior will be with you
+              every step of the way and monitoring your progress. He'll hand you
+              tasks that are meaningful to your personal development and follow
+              the industry standards.
+            </p>
+          </InfoBlockItemCss>
 
-					<InfoBlockItemCss>
-						<img src={gif2} />
-						<h1>Guaranteed growth</h1>
-						<p>
-							Then, you will be moving on to learning JavaScript by working closely
-							with very experienced engineers. A senior will be with you every step of
-							the way and monitoring your progress. He'll hand you tasks that are
-							meaningful to your personal development and follow the industry
-							standards.
-						</p>
-					</InfoBlockItemCss>
+          <InfoBlockItemCss>
+            <img src={gif3} />
+            <h1>Hands-on experience</h1>
+            <p>
+              You won't only hone your tech skills, but you'll also become a
+              solution-oriented professional that will be able to perform in a
+              highly demanding environment. An employable individual who can
+              have an impact on the front-end side of any web-based project.
+            </p>
+          </InfoBlockItemCss>
 
-					<InfoBlockItemCss>
-						<img src={gif3} />
-						<h1>Hands-on experience</h1>
-						<p>
-							You won't only hone your tech skills, but you'll also become a
-							solution-oriented professional that will be able to perform in a highly
-							demanding environment. An employable individual who can have an impact
-							on the front-end side of any web-based project.
-						</p>
-					</InfoBlockItemCss>
-
-					<InfoBlockItemCss>
-						<img src={gif4} />
-						<h1>Personal development</h1>
-						<p>
-							This is a fully remote position, so it requires a lot more self
-							discipline and communication than your usual 9-to-5 job. You'll become
-							better at managing yourself, finding innovative solutions to complex
-							problems and implementing them in a timely and organised manner. Your
-							self-learning capabilities will be intensely tested.
-						</p>
-					</InfoBlockItemCss>
-				</ItemsCss>
-				<p>
-					Do you think you have what it takes? Shoot us an email at{' '}
-					<a href="mailto:captain@kappa.london">captain@kappa.london</a> and let's discuss
-					the specifics.
-				</p>
-			</InfoBlockCss>
-		</InternshipFirstSectionWrapperCss>
-	);
+          <InfoBlockItemCss>
+            <img src={gif4} />
+            <h1>Personal development</h1>
+            <p>
+              This is a fully remote position, so it requires a lot more self
+              discipline and communication than your usual 9-to-5 job. You'll
+              become better at managing yourself, finding innovative solutions
+              to complex problems and implementing them in a timely and
+              organised manner. Your self-learning capabilities will be
+              intensely tested.
+            </p>
+          </InfoBlockItemCss>
+        </ItemsCss>
+        <p>
+          Do you think you have what it takes? Shoot us an email at{' '}
+          <a href='mailto:captain@kappa.london'>captain@kappa.london</a> and
+          let's discuss the specifics.
+        </p>
+      </InfoBlockCss>
+    </InternshipFirstSectionWrapperCss>
+  );
 };
 
 export default InternshipFirstSection;
