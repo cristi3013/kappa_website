03e8160d7c1cca3d2681ca41d600dd73df53e5a0
Cristian Vasile <vslcristian3013@gmail.com>
2020-12-22 10:42:20 +0200

    close the cards by clicking outside && no more than one card open at a time

--- src/components/Internship/section6/Card.js
+++ src/components/Internship/section6/Card.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect, useRef } from 'react'
+import ReactDOM from 'react-dom'
 import {
     CardWrapperCss,
     ImageWrapperCss,
@@ -10,13 +10,52 @@ import {
     PrimaryLinks,
 } from './index.css'
 
+function useComponentVisible(initialIsVisible) {
+    const [isComponentVisible, setIsComponentVisible] = useState(
+        initialIsVisible
+    )
+    const ref = useRef(null)
+
+    const handleHideDropdown = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+            setIsComponentVisible(false)
+        }
+    }
+
+    const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setIsComponentVisible(false)
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleHideDropdown, true)
+        document.addEventListener('click', handleClickOutside, true)
+        return () => {
+            document.removeEventListener('keydown', handleHideDropdown, true)
+            document.removeEventListener('click', handleClickOutside, true)
+        }
+    })
+
+    return { ref, isComponentVisible, setIsComponentVisible }
+}
+
 const Card = ({ cardInfo }) => {
     const { image, title, text1, text2, text3, button } = cardInfo
     const [isOpen, setIsOpen] = useState(false)
+    const {
+        ref,
+        isComponentVisible,
+        setIsComponentVisible,
+    } = useComponentVisible(true)
 
     return (
         <CardWrapperCss>
-            <div className="wrapper">
+            <div
+                className="wrapper"
+                ref={ref}
+                onClick={() => setIsComponentVisible(true)}
+            >
                 <div
                     className="box main"
                     onClick={() => setIsOpen((el) => !el)}
@@ -33,28 +72,34 @@ const Card = ({ cardInfo }) => {
                             </TitleCss>
                         </TextWrapperCss>
                     </div>
-                    <ExpandCss className="absolute" isOpen={isOpen}>
-                        {image && (
-                            <ImageWrapperCss>
-                                <img src={image} className="image" alt=""></img>
-                            </ImageWrapperCss>
-                        )}
-                        <div className="textWrapper">
-                            <TextWrapperCss>
-                                <TitleCss>
-                                    <div className="title">{title}</div>
-                                </TitleCss>
-                            </TextWrapperCss>
-                        </div>
-                        <TextCss>
-                            <div className="text">{text1}</div>
-                            <div className="text">{text2}</div>
-                            <div className="text">{text3}</div>
-                        </TextCss>
-                        <PrimaryLinks>
-                            <p>Learn now</p>
-                        </PrimaryLinks>
-                    </ExpandCss>
+                    {isComponentVisible && (
+                        <ExpandCss className="absolute" isOpen={isOpen}>
+                            {image && (
+                                <ImageWrapperCss>
+                                    <img
+                                        src={image}
+                                        className="image"
+                                        alt=""
+                                    ></img>
+                                </ImageWrapperCss>
+                            )}
+                            <div className="textWrapper">
+                                <TextWrapperCss>
+                                    <TitleCss>
+                                        <div className="title">{title}</div>
+                                    </TitleCss>
+                                </TextWrapperCss>
+                            </div>
+                            <TextCss>
+                                <div className="text">{text1}</div>
+                                <div className="text">{text2}</div>
+                                <div className="text">{text3}</div>
+                            </TextCss>
+                            <PrimaryLinks>
+                                <p>Learn now</p>
+                            </PrimaryLinks>
+                        </ExpandCss>
+                    )}
                 </div>
             </div>
         </CardWrapperCss>
